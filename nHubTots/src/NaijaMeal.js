b0a0a2acb7b92e9a6c034c457bfb85bc21e77108
Abdulkadir <uabdulkadir06@gmail.com>
2025-04-13 20:20:25 +0100

    added meal data

---
+++ nHubTots/src/NaijaMeal.js
@@ -0,0 +1,122 @@
+export const meals =[
+    {
+        id: 1,
+        name: 'Pap and Akara',
+        img:{
+            src: 'https://opensauce.vendease.com/wp-content/uploads/2023/05/image-10.png',
+            alt:'pap and akara'
+        },
+        mealType: 'Breakfast'
+    },
+    {
+        id: 2,
+        name: 'Masa',
+        img:{
+            src: 'https://yellowplate.ng/wp-content/uploads/2023/04/STUFFED-BEEF-MASA-WITH-PEANUT-SAUCE.png',
+            alt:'masa'
+        },
+        mealType: 'Breakfast'
+    },
+    {
+        id: 3,
+        name: 'Gurasa',
+        img:{
+            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOyWfmfDGNg-U9siLWHLs0p9iK7XK1Zd69nzcZjKiWSFnJXUAxJdZTSQXKfpzps6K7dsc&usqp=CAU',
+            alt:'gurasa'
+        },
+        mealType: 'Breakfast'
+    },
+    
+    {
+        id: 4,
+        name: 'Yam and Egg Sauce',
+        img:{
+            src: 'https://opensauce.vendease.com/wp-content/uploads/2023/05/image-12-1024x576.png',
+            alt:'yam and egg sauce'
+        },
+        mealType: 'Breakfast'
+    },
+    {
+        id: 5,
+        name: 'Boiled Plantain and Egg Sauce',
+        img:{
+            src: 'https://opensauce.vendease.com/wp-content/uploads/2023/05/image-11.png',
+            alt:'boiled plantain and egg sauce'
+        },
+        mealType: 'Breakfast'
+    },
+    {
+        id: 6,
+        name: 'Beans',
+        img:{
+            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6Br6UTFC3ZAgOTjRd2g_i9zwmJzmg72q12g&s',
+            alt:'Beans'
+        },
+        mealType: 'Breakfast, Launch, Dinner'
+    },
+    {
+        id: 7,
+        name: 'Moi moi',
+        img:{
+            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiC3NyOSp7JpfZtO0BvWpyrlgb67eCTQKvAA&s',
+            alt:'Moi moi'
+        },
+        mealType: 'Breakfast, Launch, Dinner'
+    },
+    {
+        id: 8,
+        name: 'Dan wake',
+        img:{
+            src: 'https://img-global.cpcdn.com/recipes/cc659ae3d3b83b0b/680x964cq70/danwake-recipe-main-photo.webp',
+            alt:'Dan wake'
+        },
+        mealType: 'Breakfast, Launch, Dinner'
+    },
+    {
+        id: 9,
+        name: 'Jollof RIce',
+        img:{
+            src: 'https://allnigerianfoods.com/wp-content/uploads/jollof-rice.jpg',
+            alt:'jollof rice'
+        },
+        mealType: 'Launch and Dinner'
+    },
+    {
+        id: 10,
+        name: 'Rice and Beans',
+        img:{
+            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzuI-4qbTvqTWXo0dq_hc4iOa95Pq88t5YOg&s',
+            alt:'rice and beans'
+        },
+        mealType: 'Launch and Dinner'
+    },
+    {
+        id: 11,
+        name: 'Fufu and Egusi',
+        img:{
+            src: 'https://allnigerianfoods.com/wp-content/uploads/egusiandfufu.jpg',
+            alt:'fufu and egusi'
+        },
+        mealType: 'Launch and Dinner'
+    },
+    {
+        id: 12,
+        name: 'Spaghetti',
+        img:{
+            src: 'https://veganwithgusto.com/wp-content/uploads/2021/05/spicy-spaghetti-arrabbiata.jpg',
+            alt:'spaghetti'
+        },
+        mealType: 'Launch and Dinner'
+    },
+    {
+        id: 13,
+        name: 'Gwate',
+        img:{
+            src: 'https://img-global.cpcdn.com/recipes/d058676ea6ae3fee/680x964cq70/fate-maize-and-vegetable-porridge-recipe-main-photo.webp',
+            alt:'gwate'
+        },
+        mealType: 'Launch and Dinner'
+    },
+
+    
+]
